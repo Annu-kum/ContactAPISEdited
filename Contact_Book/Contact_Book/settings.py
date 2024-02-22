c9cf67b8129b16fce5dc84643d2dd87523d4cd0e
Annu-kum <annuk5523@gmail.com>
2024-02-22 12:25:08 +0530

    modify files

--- Contact_Book/Contact_Book/settings.py
+++ Contact_Book/Contact_Book/settings.py
@@ -74,14 +74,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Contact_Book.wsgi.application'
 REST_FRAMEWORK = {
 
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
         
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-       'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-      'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ),
+    #    'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
+    #   'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
 }
 
 
--- Contact_Book/contactBook_api_/urls.py
+++ Contact_Book/contactBook_api_/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 from rest_framework import routers
 
-
-
 router = routers.DefaultRouter()
 
 urlpatterns = [
